3af531a53db2cd8b92df7e6c108d92df1c926c05
ThiruvRaja <131310561+ThiruvRaja@users.noreply.github.com>
2023-04-19 16:09:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+function message() {
+    document.getElementById("goal").innerHTML = "We strive towards to offer quality food at a reasonable price.";
+}
+function description() {
+    document.getElementById("detail").innerHTML = "A famous crepe dish from South Indian cuisine which is made with rice batter.";
+}
+function hide() {
+    document.getElementById("detail").innerHTML = "";
+}
+function description1() {
+    document.getElementById("detail1").innerHTML = "A different version of the traditional mughal briyani.";
+}
+function hide1() {
+    document.getElementById("detail1").innerHTML = "";
+}
+function description2() {
+    document.getElementById("detail2").innerHTML = "Savoury Rice cakes, a popular breakfast item from South India.";
+}
+function hide2() {
+    document.getElementById("detail2").innerHTML = "";
+}
+function description3() {
+    document.getElementById("detail3").innerHTML = "Layered flatbread made with all purpose flour.";
+}
+function hide3() {
+    document.getElementById("detail3").innerHTML = "";
+}
+function description4() {
+    document.getElementById("detail4").innerHTML = "Pongal is a delicious South Indian porridge like dish that is made with rice and yellow moong lentils.";
+}
+function hide4() {
+    document.getElementById("detail4").innerHTML = "";
+}
+function description5() {
+    document.getElementById("detail5").innerHTML = "Medu Vada is a popular South Indian breakfast snack of donut shaped lentil fritters that are fluffy, crispy, soft and delicious.";
+}
+function hide5() {
+    document.getElementById("detail5").innerHTML = "";
+}
+function description6() {
+    document.getElementById("detail6").innerHTML = "Thinly shredded roti mixed with the curry, vegetables and Sri Lankan aromatic spices.";
+}
+function hide6() {
+    document.getElementById("detail6").innerHTML = "";
+}
+function description7() {
+    document.getElementById("detail7").innerHTML = "A flavorful Indian rice dish with bell peppers, peas, potatoes in a spiced rice dish made with warm indian spices.";
+}
+function hide7() {
+    document.getElementById("detail7").innerHTML = "";
+}
+function description8() {
+    document.getElementById("detail8").innerHTML = "Mysore Pak is a sweet delicacy made with chickpea flour (besan) and melted ghee.";
+}
+function hide8() {
+    document.getElementById("detail8").innerHTML = "";
+}
+function description9() {
+    document.getElementById("detail9").innerHTML = "A dish made from ball-shaped dumplings of chhena and semolina dough, cooked in light sugar syrup made of sugar.";
+}
+function hide9() {
+    document.getElementById("detail9").innerHTML = "";
+}
+function description10() {
+    document.getElementById("detail10").innerHTML = "Small spongy milky balls soaked in rose scented syrup.";
+}
+function hide10() {
+    document.getElementById("detail10").innerHTML = "";
+}
+function description11() {
+    document.getElementById("detail11").innerHTML = "A popular, delicious South Indian sweet dish made with semolina, sugar, ghee, saffron, nuts and dry fruits.";
+}
+function hide11() {
+    document.getElementById("detail11").innerHTML = "";
+}
+function description12() {
+    document.getElementById("detail12").innerHTML = "Beverage made with tea leaves, popular throughout India and other parts of Asia.";
+}
+function hide12() {
+    document.getElementById("detail12").innerHTML = "";
+}
+function description13() {
+    document.getElementById("detail13").innerHTML = "Indian Spiced Coffee is a milk coffee recipe; which is made with a blend of ground spices, milk, coffee powder and sugar.";
+}
+function hide13() {
+    document.getElementById("detail13").innerHTML = "";
+}
+function description14() {
+    document.getElementById("detail14").innerHTML = "A delicious creamy drink made with mango, yogurt, milk, a little sugar, and a sprinkling of cardamom.";
+}
+function hide14() {
+    document.getElementById("detail14").innerHTML = "";
+}
+function description15() {
+    document.getElementById("detail15").innerHTML = "Juice made from freshly selected grapes.";
+}
+function hide15() {
+    document.getElementById("detail15").innerHTML = "";
+}
